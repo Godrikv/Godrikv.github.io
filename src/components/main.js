1397aa60a023b346781ab3970b2cc5c0f3edaef2
Godrikv <SinfopsS@mail.ru>
2020-07-18 21:16:40 +0300

    1

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,17 @@
 "use strict";
-let getAudio = document.querySelector('.audio_car');
+//видео
 let getVideo = document.querySelector('.header__main_center_video');
 getVideo.addEventListener('ended', function f() {let audio = new Audio(); // Создаём новый элемент Audio
     audio.src = '../src/assets/audio/audio.mp3'; // Указываем путь к звуку "клика"
     audio.autoplay = true; // Автоматически запускаем
+});
+
+//шины
+let shins = document.querySelectorAll('.shins');
+getVideo.addEventListener('ended', function () {
+
+
+        shins.style.opacity = '0';
+        
+
 });
