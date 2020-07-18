91bad42eebf4b47dcdf13cf1635adf9dcdc8c1ed
Godrikv <SinfopsS@mail.ru>
2020-07-18 21:38:52 +0300

    2

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,13 @@
 "use strict";
 //видео
 let getVideo = document.querySelector('.header__main_center_video');
-let shins = document.querySelectorAll('.shins');
 getVideo.addEventListener('ended', function f() {let audio = new Audio(); // Создаём новый элемент Audio
     audio.src = '../src/assets/audio/audio.mp3'; // Указываем путь к звуку "клика"
     audio.autoplay = true; // Автоматически запускаем
-    shins.style.opacity = '0';
 });
 
 //шины
-
+let shins = document.querySelectorAll('.shins');
+getVideo.addEventListener('ended', function () {
+    shins.forEach(element => shins.style.opacity = '0')
+});
