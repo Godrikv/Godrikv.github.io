c7f1ed4cdf845a631d7d28508d78b4dd2854f455
Godrikv <SinfopsS@mail.ru>
2020-07-18 21:45:06 +0300

    2

--- src/components/main.js
+++ src/components/main.js
@@ -4,10 +4,8 @@ let getVideo = document.querySelector('.header__main_center_video');
 getVideo.addEventListener('ended', function f() {let audio = new Audio(); // Создаём новый элемент Audio
     audio.src = '../src/assets/audio/audio.mp3'; // Указываем путь к звуку "клика"
     audio.autoplay = true; // Автоматически запускаем
+    shins.forEach(element => shins.style.opacity = '0')
 });
 
 //шины
 let shins = document.querySelectorAll('.shins');
-getVideo.addEventListener('ended', function fe() {
-    shins.forEach(element => shins.style.opacity = '0')
-});
