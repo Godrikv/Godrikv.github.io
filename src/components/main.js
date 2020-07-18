2954174dc37ec815e081c179463b6415453b746a
Godrikv <SinfopsS@mail.ru>
2020-07-18 21:48:07 +0300

    3

--- src/components/main.js
+++ src/components/main.js
@@ -4,8 +4,8 @@ let getVideo = document.querySelector('.header__main_center_video');
 getVideo.addEventListener('ended', function f() {let audio = new Audio(); // Создаём новый элемент Audio
     audio.src = '../src/assets/audio/audio.mp3'; // Указываем путь к звуку "клика"
     audio.autoplay = true; // Автоматически запускаем
-    shins.forEach(element => shins.style.opacity = '0')
+    shins.style.opacity = '0';
 });
 
 //шины
-let shins = document.querySelectorAll('.shins');
+let shins = document.querySelector('.shins');
