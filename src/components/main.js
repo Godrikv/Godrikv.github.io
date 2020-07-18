e2557670786df70fb0f3867920079f4bcf5e3daf
Godrikv <SinfopsS@mail.ru>
2020-07-18 18:36:26 +0300

    1

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 "use strict";
 let getAudio = document.querySelector('.audio_car');
 let getVideo = document.querySelector('.header__main_center_video');
-getVideo.addEventListener('ended', function f() {getAudio.setAttribute('autoplay', 'autoplay')
+getVideo.addEventListener('ended', function f() {getAudio.setAttribute('autoplay')
 
 });
