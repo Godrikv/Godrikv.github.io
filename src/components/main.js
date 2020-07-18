999cd159b66dd92c75414519b1dd0942b647470a
Godrikv <SinfopsS@mail.ru>
2020-07-18 18:46:41 +0300

    1

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,7 @@
 "use strict";
 let getAudio = document.querySelector('.audio_car');
 let getVideo = document.querySelector('.header__main_center_video');
-getVideo.addEventListener('ended', function f() {getAudio.setAttribute('autoplay')
-
+getVideo.addEventListener('ended', function f() {let audio = new Audio(); // Создаём новый элемент Audio
+    audio.src = '../src/assets/audio/audio.mp3'; // Указываем путь к звуку "клика"
+    audio.autoplay = true; // Автоматически запускаем
 });
