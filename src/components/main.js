20125ab8e7ffd9d9dbda9b8b64f2cf0b3eec770e
Godrikv <SinfopsS@mail.ru>
2020-07-26 19:13:41 +0300

    2

--- src/components/main.js
+++ src/components/main.js
@@ -6,7 +6,7 @@ getVideo.addEventListener('ended', function f() {
     audio.src = '../src/assets/audio/audio.mp3';
     audio.autoplay = true;
     document.querySelectorAll('.shins').forEach(function (item) {
-item.style.opacity = '0'
+item.style.opacity = '1'
     });
     document.querySelector('.header__main').style.backgroundImage = 'url("../src/assets/img/fon.png")'
 });
