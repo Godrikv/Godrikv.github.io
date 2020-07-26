2f39bfa1edfbf8be86954005b44d738195558bc3
Godrikv <SinfopsS@mail.ru>
2020-07-26 15:32:41 +0300

    flag/bg

--- src/components/main.js
+++ src/components/main.js
@@ -8,6 +8,7 @@ getVideo.addEventListener('ended', function f() {
     document.querySelectorAll('.shins').forEach(function (item) {
 item.style.opacity = '0'
     });
+    document.querySelector('.header__main').style.backgroundImage = 'url("../img/fon.png")'
 });
 
 //шины
